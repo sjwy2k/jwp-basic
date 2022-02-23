fcfe232ef5efb747ed91bd75a4c5124ea086fc6f
Jay Shin <sjwy2k@gmail.com>
2022-02-24 03:39:50 +0900

    220224 jwp 6.1.2 개인정보수정 실습 UpdateUserFormServlet, UpdateUserProceedServlet 진행 중

---
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -0,0 +1,29 @@
+package next.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import core.db.DataBase;
+import next.model.User;
+
+@WebServlet("/user/updateRequest")
+public class UpdateUserFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	User user = DataBase.findUserById(req.getParameter("userId"));
+    	log.debug("user : {}", user);
+    	resp.sendRedirect("/user/update");
+    }
+    
+}
